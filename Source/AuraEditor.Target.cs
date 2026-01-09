d19637e7116c297c6ad133e156557bc97304feba
unknown <feixiaojie@cn.net.ntes>
2026-01-09 11:47:42 +0800

    satisfy ue5.6

--- Source/AuraEditor.Target.cs
+++ Source/AuraEditor.Target.cs
@@ -8,7 +8,8 @@ public class AuraEditorTarget : TargetRules
 	public AuraEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		CppStandard = CppStandardVersion.Cpp20;
 
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
